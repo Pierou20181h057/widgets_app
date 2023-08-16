9b7562a8c62dd29489e6d49aee81ac81b6745076
Piero20181h057 <u20181h057@upc.edu.pe>
2023-08-16 17:26:05 -0500

    buttons screen finish

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -56,6 +56,9 @@ class _ButtonsView extends StatelessWidget {
               TextButton(onPressed: (){}, child: const Text('Text')),
               TextButton.icon(onPressed: (){}, icon: const Icon(Icons.access_alarms_rounded),
               label: const Text('Text icon')),
+
+              const CustomButton(),
+
               IconButton(onPressed: (){}, icon: const Icon(Icons.access_alarms_rounded)),
               IconButton(onPressed: (){}, icon: const Icon(Icons.access_alarms_rounded),
               style: ButtonStyle(
@@ -64,6 +67,30 @@ class _ButtonsView extends StatelessWidget {
               ),),
           ],
         )
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: (){
+            
+          },
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hola mundo', style: TextStyle(color: Colors.white),)
+            ),
+        ),
       ),
     );
   }
