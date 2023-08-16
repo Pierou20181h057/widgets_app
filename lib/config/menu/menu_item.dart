30dc284240e77287bd29618a15efd2403902d8f0
Piero20181h057 <u20181h057@upc.edu.pe>
2023-08-16 09:55:31 -0500

    rutas basicas configuradas

--- lib/config/menu/menu_item.dart
+++ lib/config/menu/menu_item.dart
@@ -23,7 +23,7 @@ List<MenuItem> appMenuItems = <MenuItem>[
     MenuItem(
     title: 'Tarjetas', 
     subTitle: 'Contenedor estilizado', 
-    link: '/card', 
+    link: '/cards', 
     icon: Icons.credit_card),
 
 ];
--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -45,11 +46,18 @@ class _CustomListTItle extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context).colorScheme;
     return ListTile(
-      leading: Icon(menuItem.icon, color: theme.primary,),
-      trailing: Icon(Icons.arrow_forward_ios_rounded, color: theme.primary,),
+      leading: Icon(
+        menuItem.icon,
+        color: theme.primary,
+      ),
+      trailing: Icon(
+        Icons.arrow_forward_ios_rounded,
+        color: theme.primary,
+      ),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap: (){
+      onTap: () {
+        Navigator.pushNamed(context, menuItem.link);
         
       },
     );
